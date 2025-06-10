96e91efed22438dcb02741cd7d5b88de19b97903
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-06-10 04:01:17 +0100

    Azeem landing page

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -6,7 +6,7 @@ import { Menu, X, ChevronDown, Search } from 'lucide-react';
 import Link from 'next/link';
 
 const topLinks = ['SUBSCRIBE', 'RESOURCES', 'CAREERS'];
-const bottomLinks = [{name:'About Us', link:'/solution'}, {name:'Projects',link:'/project'}, {name:'News & Events', link:'/news'}, {name:'Partnerships', link:'/partners'}];
+const bottomLinks = [{name:'Solutions', link:'/solution'}, {name:'Projects',link:'/project'}, {name:'News & Events', link:'/news'}, {name:'Partnerships', link:'/partners'}, {name:'About Us', link:'/about'}];
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = useState(false);
