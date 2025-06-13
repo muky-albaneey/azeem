c25ad13d74db38af5309ed6d3970ce6fb8098d1f
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2025-06-13 21:10:40 +0100

    Azeem landing page

--- src/components/about/AboutHeader.tsx
+++ src/components/about/AboutHeader.tsx
@@ -4,9 +4,9 @@ export default function AboutHeader() {
   return (
      <header className="bg-[#e6e1c5] shadow-md sticky top-0 z-50 hidden md:block">
           <div className="max-w-7xl mx-auto px-4 md:px-8 py-3 flex justify-center gap-6 text-sm md:text-base font-semibold text-gray-800">
-            <a href="/solution/approach" className="hover:underline">Who we are</a>
-            <a href="/solution/impact" className="hover:underline">what we do</a>
-            <a href="/solution/research" className="hover:underline">Our Team</a>
+            <a href="#" className="hover:underline">Who we are</a>
+            <a href="#" className="hover:underline">what we do</a>
+            <a href="#" className="hover:underline">Our Team</a>
             <a href="#" className="hover:underline">Our Board of Directors</a>
             <a href="#" className="hover:underline">Our History</a>
           </div>
